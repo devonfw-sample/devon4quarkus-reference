7fc87875dc6c9c77054d58e85b10faf06de65954
Julian Günther <julian.guenther@capgemini.com>
2021-12-02 14:20:17 +0100

    changed visibility in AbstractExceptionMapper to protected

--- src/main/java/com/devonfw/quarkus/general/service/exception/mapper/AbstractExceptionMapper.java
+++ src/main/java/com/devonfw/quarkus/general/service/exception/mapper/AbstractExceptionMapper.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class AbstractExceptionMapper {
 
-  private boolean exposeInternalErrorDetails = false;
+  protected boolean exposeInternalErrorDetails = false;
 
   @Context
   UriInfo uriInfo;
